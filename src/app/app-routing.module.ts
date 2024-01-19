08d42fbf30d568cfdea06a51602cccecc669b2a5
jearth <pop08414@gmail.com>
2024-01-19 17:41:49 +0900

    2024.01.19

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from './Content/start/start.component';
+import { RegisterComponent } from './Content/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'start', component: StartComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './Content/start/start.component';
+import { FooterComponent } from './Layout/footer/footer.component';
+import { HeaderComponent } from './Layout/header/header.component';
+import { SidebarComponent } from './Layout/sidebar/sidebar.component';
+import { RegisterComponent } from './Content/register/register.component';
+import { EditComponent } from './Content/edit/edit.component';
+import { DetailComponent } from './Content/detail/detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    RegisterComponent,
+    EditComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
